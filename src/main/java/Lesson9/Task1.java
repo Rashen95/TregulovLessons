74e4f62fc3fe2a781ebae355480ea8fc43f3501a
Artem Privalov <rashen95@mail.ru>
2023-07-31 21:24:18 +0300

    HomeWork 9 complete v2

--- src/main/java/Lesson9/Task1.java
+++ src/main/java/Lesson9/Task1.java
@@ -1,23 +1,18 @@
 package Lesson9;
 
 public class Task1 {
+    static void createObject() {
+        Task1 t1 = new Task1();
+        Task1 t2 = new Task1();
+    }
     public static void main(String[] args) {
         // Создаем объекты
         Task1 t1 = new Task1();
         Task1 t2 = new Task1();
-        Task1 t3 = new Task1();
-        Task1 t4 = new Task1();
-        Task1 t5 = new Task1();
-        Task1 t6 = new Task1();
-        Task1 t7 = new Task1();
-        Task1 t8 = new Task1();
 
-        // Обнуляем объекты
-        t3 = null;
-        t4 = null;
-        t5 = null;
-        t6 = null;
-        t7 = null;
-        t8 = null;
+        // Создаем объекты через методы и они заканчивают свое существование в конце работы методов
+        createObject();
+        createObject();
+        createObject();
     }
 }
