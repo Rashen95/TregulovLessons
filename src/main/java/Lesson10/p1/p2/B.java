c97016bbdc6463de23117b55cd3408aa6cc442b8
Artem Privalov <rashen95@mail.ru>
2023-08-01 22:00:23 +0300

    HomeWork 10 complete

---
+++ src/main/java/Lesson10/p1/p2/B.java
@@ -0,0 +1,6 @@
+package Lesson10.p1.p2;
+
+public class B {
+    public static int a = 15;
+    public static boolean b = true;
+}
---
+++ src/main/java/Lesson10/p4/D.java
@@ -0,0 +1,18 @@
+package Lesson10.p4;
+
+import Lesson10.p1.*;
+
+import static Lesson10.p1.p2.B.*;
+
+import Lesson10.p1.p2.p3.*;
+import Lesson10.p4.p5.*;
+
+public class D {
+    public static void main(String[] args) {
+        System.out.println(new A().i1);
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(new C().d1);
+        System.out.println(new E().b1);
+    }
+}
